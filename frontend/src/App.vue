7692949068e1c8181180af81c9fc37e3ae209d78
felixbouazza <digitalmine.france@gmail.com>
2021-03-23 17:35:17 +0100

    Add sign-up and log-in button who redirect to login and signup page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,62 @@
 <template>
-  <div id="nav">
-  </div>
-  <router-view/>
+  <nav>
+    <router-link to="/"><strong>JumpRopeCounter</strong></router-link>
+    <div class="navbarlist">
+      <template v-if="$store.state.isAuthenticated">
+        <router-link to="/dashboard">Dashboard</router-link>
+      </template>
+
+      <template v-else>
+        <router-link to="/">Home</router-link>
+        <div class="items">
+          <router-link to="/sign-up" id="sign-up"><strong>Sign up</strong></router-link>
+          <router-link to="/log-in" id="log-in"><strong>Log in</strong></router-link> 
+        </div>
+      </template>
+    </div>
+  </nav>
+  <section class="section">
+    <router-view/>
+  </section>
+
+  <footer class="footer">
+    <p>Copyright (c) 2021</p>
+  </footer>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
 
-export default {
-  name: 'App',
-
-}
+  export default {
+    name: 'App',
+    beforeCreate() {
+      this.$store.commit('initializeStore')
+      
+      const token = this.$store.state.token
+      if (token) {
+        axios.defaults.headers.common['Authorization'] = 'Token' + token
+      } else {
+        axios.defaults.headers.common['Authorization'] = ''
+      }
+    }
+  }
 </script>
 
 
 <style lang="scss">
+#sign-up {
+  background-color: green;
+  border-radius: 5%;
+  padding: 4px 8px;
+  color: white;
+  text-decoration: none;
+}
 
+#log-in {
+  background-color: red;
+  border-radius: 5%;
+  padding: 4px 8px;
+  color: white;
+  text-decoration: none;
+}
 </style>
